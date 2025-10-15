c0be0298605db3504856870ff482d78d31e4e7f1
Mosab Ahmed Irwished <42383497+Mosab97@users.noreply.github.com>
2025-10-15 18:50:18 +0300

    get product by slug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -84,4 +84,14 @@ class Product extends Model
         return $query->where('name', 'like', '%' . $value . '%')
             ->orWhere('description', 'like', '%' . $value . '%');
     }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
