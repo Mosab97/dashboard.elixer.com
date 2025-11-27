8ceb6f293e1f2ac9599ed4b8feb2a402917aba6b
Mosab Ahmed Irwished <42383497+Mosab97@users.noreply.github.com>
2025-11-27 12:53:11 +0200

    Update _page-loader.blade.php

--- resources/views/CP/metronic/layout/partials/_page-loader.blade.php
+++ resources/views/CP/metronic/layout/partials/_page-loader.blade.php
@@ -1,7 +1,7 @@
 <!--begin::loader-->
 <div class="page-loader flex-column">
-    <img alt="Logo" class="theme-light-show max-h-50px" src="{{ asset('media/logos/logo-opts-small-test.png') }}" />
-    <img alt="Logo" class="theme-dark-show max-h-50px" src="{{ asset('media/logos/logo-opts-light-small-test.png') }}" />
+    <img alt="Logo" class="theme-light-show max-h-50px" style="width: 100px;" src="{{ asset('media/logos/logo.png') }}" />
+    <img alt="Logo" class="theme-dark-show max-h-50px" style="width: 100px;" src="{{ asset('media/logos/logo.png') }}" />
     <div class="d-flex align-items-center mt-5">
         <span class="spinner-border text-primary" role="status"></span>
         <span class="text-muted fs-6 fw-semibold ms-5">Loading...</span>
